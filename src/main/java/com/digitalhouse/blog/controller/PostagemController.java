97dda3a122bcb9704e69b6a797a399cc4cc14076
erictomson <erictomson@gmail.com>
2021-10-15 15:59:33 -0300

    Implementados métodos para pesquisa, inserção, alteração e exclusão de postagens.

---
+++ src/main/java/com/digitalhouse/blog/controller/PostagemController.java
@@ -0,0 +1,68 @@
+package com.digitalhouse.blog.controller;
+
+import com.digitalhouse.blog.model.Postagem;
+import com.digitalhouse.blog.repository.PostagemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+// Definindo a classe como um controller
+@RestController
+// Mapeando Postagem
+@RequestMapping("/postagens")   // Ajuste da pagina de carregamento
+// Captura qualquer referência a Postagem
+@CrossOrigin("*")   // Para não ter problemas com as origens, seja react, angular etc.
+public class PostagemController {
+
+    @Autowired      // Como é uma interface deixamos o trabalho da injeção a cargo do proprio Spring
+    private PostagemRepository repository;  // Injetando o repositório
+
+    @GetMapping  // Sempre que vir algo externo através da URL, será tratado aqui
+
+    public ResponseEntity<List<Postagem>> GetAll() {
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Postagem> GetById(@PathVariable Long id) {
+        // Precisamos usar dentro da passagem do parâmetro a anotação PathVariable pra dizer
+        // ao Spring que cuide da parte de trazer da url a informação e use com parametro.
+        return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
+                .orElse(ResponseEntity.notFound().build());
+        // Se der tudo certo o objeto encontrado vem no responseEntity.om
+        // pode não existir então o orElse informar o not found
+    }
+
+    // Pesquisar pelo título
+    @GetMapping("titulo/{titulo}")
+    // Lembrando, podemos ter 1 postagem ou mais de uma postagem com o titulo
+    // por isso nesse caso lembrando o primeiro mapping nós iremos fazer uma
+    // List do tipo postagem para este retorno
+    public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo) {
+        return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+    }
+
+    // Pesquisar pelo texto
+    @GetMapping("texto/{texto}")
+    public ResponseEntity<List<Postagem>> GetByTexto(@PathVariable String texto) {
+        return ResponseEntity.ok(repository.findAllByTextoContainingIgnoreCase(texto));
+    }
+
+    @PostMapping
+    public ResponseEntity<Postagem> post(@RequestBody Postagem postagem) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+    }
+
+    @PutMapping
+    public ResponseEntity<Postagem> put(@RequestBody Postagem postagem) {
+        return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/digitalhouse/blog/model/Postagem.java
@@ -0,0 +1,77 @@
+package com.digitalhouse.blog.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Date;
+
+// Tornando a classe uma entidade gerenciavel
+// Esta entidade se tornará uma tabela no database
+@Entity
+// Necessário serializar a classe para que os dados
+// sejam trocadas de forma binária (e não objetos Java)
+// @Table(name="tb_blog_model")        // Definindo o nome da tabela
+public class Postagem implements Serializable {
+
+    // A cada instância, será gerado um identificador
+    // único para a classe
+    private static long serialVersionUID = 1L;
+
+    // Definindo a chave primária (PK)
+    @Id
+    // Gerando a chave primária
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    // @Column(name="post_titulo")     // Determinando o nome da tabela
+    @NotNull                        // Definindo como não nulo
+    @Size(min=5,max=100)            // Limites mínimo e máximo de caracteres
+    private String titulo;
+    // @Column(name="post_texto")      // Determinando o nome da tabela
+    @NotNull                        // Definindo como não nulo
+    @Size(min=5,max=500)            // Limites mínimo e máximo de caracteres
+    private String texto;
+    // @Column(name="post_data")      // Determinando o nome da tabela
+    @Temporal(TemporalType.TIMESTAMP)   // Insere a data atual
+    private Date data = new java.sql.Date(System.currentTimeMillis());
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public static void setSerialVersionUID(long serialVersionUID) {
+        Postagem.serialVersionUID = serialVersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Date getDate() {
+        return data;
+    }
+
+    public void setDate(Date date) {
+        this.data = date;
+    }
+}
